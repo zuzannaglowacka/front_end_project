c89c8f74ac34097da6df03e9e9feb56f4f925624
Zuzanna Glowacka <zuzglowacka@gmail.com>
2018-08-25 18:55:32 +0100

    navbar colour change on scroll with jquery done

---
+++ assets/javascripts/scroll.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     if($(document).scrollTop() > 70) {
+//       $('#navbar').addClass('white');
+//     }
+//     else {
+//     $('#navbar').removeClass('white');
+//     }
+//   });
+// });
